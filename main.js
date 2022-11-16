5962dee5baabc98a233dd49ba8a8598b27b3f2da
YangYang <liu@YangYang.local>
2022-11-16 20:27:31 +0800

    完成了首页的功能开发

--- main.js
+++ main.js
@@ -2,7 +2,28 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
-
+//导入网络请求的包
+import {$http} from '@escook/request-miniprogram'
+uni.$http =$http
+$http.baseUrl = 'https://www.uinav.com'
+//发送请求前 请求拦截器
+$http.beforeRequest=function(options){
+  //展示loading的效果
+  uni.showLoading({
+    title:"数据加载中"
+  })
+}
+//发送请求后 响应拦截器
+$http.afterRequest = function(options){
+  uni.hideLoading()
+}
+uni.$showMsg = function(title = "数据加载失败",duration="1500"){
+  uni.showToast({
+    title:"数据加载失败...",
+    duration:1500,
+    icon:"none"
+  })
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
